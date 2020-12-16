ca514b8b3ec09943a8f8e7f629e181c0915d443c
liqibo <1289758842@qq.com>
2020-12-16 18:49:24 +0800

    v2

--- src/main/java/com/atguigu/Demo/Demo1.java
+++ src/main/java/com/atguigu/Demo/Demo1.java
@@ -1,6 +1,7 @@
 package com.atguigu.Demo;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
 
 
 import java.util.List;
@@ -8,44 +9,49 @@ import java.util.Set;
 
 public class Demo1 {
     public static void main(String[] args) {
-        Jedis jedis = Jedis_util.getJedisFromPool();
-        String set = jedis.set("k1", "v1");
-        String set1 = jedis.set("k2", "v2");
-        String set2 = jedis.set("k3", "v3");
-        Set<String> keys = jedis.keys("*");
-        System.out.println(keys.size());
-        for (String key : keys) {
-            System.out.println(key);
-        }
+//        Jedis jedis = Jedis_util.getJedisFromPool();
+//        String set = jedis.set("k1", "v1");
+//        String set1 = jedis.set("k2", "v2");
+//        String set2 = jedis.set("k3", "v3");
+//        Set<String> keys = jedis.keys("*");
+//        System.out.println(keys.size());
+//        for (String key : keys) {
+//            System.out.println(key);
+//        }
+//
+//        System.out.println(jedis.exists("k1")); //k1是否存在
+//
+//
+//        System.out.println(jedis.get("k1"));  //获得k1的值
+//
+//        jedis.mset("st1", "v1", "st2", "v2");
+//        List<String> mget = jedis.mget("st1", "st2");
+//        System.out.println(mget);
+//
+//        jedis.lpush("l2","1","2","3");
+//        List<String> l2 = jedis.lrange("l2", 0, -1);
+//        System.out.println(l2);
+//        System.out.println(jedis.llen("l2"));
+//        Long l21 = jedis.llen("l2");
+//        for (int i = 0; i < l21; i++) {
+//            jedis.lpop("l2");
+//        }
+//
+//        jedis.sadd("orders","order01");
+//        jedis.sadd("orders","order02");
+//        jedis.sadd("orders","order03");
+//        jedis.sadd("orders","order04");
+//        jedis.srem("orders","order01");
+//        Set<String> orders = jedis.smembers("orders");
+//        System.out.println(orders);
+//
+//        jedis.hset("hash1", "username", "lisi");
+//
+//        System.out.println(jedis.hget("hash1", "username"));
+//        jedis.close();
 
-        System.out.println(jedis.exists("k1")); //k1是否存在
-
-
-        System.out.println(jedis.get("k1"));  //获得k1的值
-
-        jedis.mset("st1", "v1", "st2", "v2");
-        List<String> mget = jedis.mget("st1", "st2");
-        System.out.println(mget);
-
-        jedis.lpush("l2","1","2","3");
-        List<String> l2 = jedis.lrange("l2", 0, -1);
-        System.out.println(l2);
-        System.out.println(jedis.llen("l2"));
-        Long l21 = jedis.llen("l2");
-        for (int i = 0; i < l21; i++) {
-            jedis.lpop("l2");
-        }
-
-        jedis.sadd("orders","order01");
-        jedis.sadd("orders","order02");
-        jedis.sadd("orders","order03");
-        jedis.sadd("orders","order04");
-        jedis.srem("orders","order01");
-        Set<String> orders = jedis.smembers("orders");
-        System.out.println(orders);
-
-        jedis.hset("hash1", "username", "lisi");
-
-        jedis.close();
+        JedisCluster jedisCluster = Jedis_util.getJedisCluster();
+        jedisCluster.set("k100","v101");
+        System.out.println(jedisCluster.get("k100"));
     }
 }
--- src/main/java/com/atguigu/Demo/Jedis_util.java
+++ src/main/java/com/atguigu/Demo/Jedis_util.java
@@ -1,8 +1,9 @@
 package com.atguigu.Demo;
 
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class Jedis_util {
     private static JedisPool jedisPool=null;
@@ -16,11 +17,37 @@ public class Jedis_util {
             jedisPoolConfig.setMaxWaitMillis(2000); //等待时间
             jedisPoolConfig.setTestOnBorrow(true); //取连接的时候进行一下测试 ping pong
 
-            jedisPool=new JedisPool(jedisPoolConfig,"192.168.11.103", 6379 );
+            jedisPool=new JedisPool(jedisPoolConfig,"hadoop102", 6379 );
             return jedisPool.getResource();
         }else{
             return jedisPool.getResource();
         }
 
     }
+
+    private static   JedisCluster jedisCluster=null;
+
+    public static JedisCluster getJedisCluster(){
+        if(jedisCluster==null){
+
+            Set<HostAndPort> hostAndPortSet =new HashSet<>();
+            hostAndPortSet.add(new HostAndPort("hadoop102",6390));
+            hostAndPortSet.add(new HostAndPort("hadoop102",6391));
+
+            JedisPoolConfig jedisPoolConfig =new JedisPoolConfig();
+            jedisPoolConfig.setMaxTotal(10); //最大可用连接数
+            jedisPoolConfig.setMaxIdle(5); //最大闲置连接数
+            jedisPoolConfig.setMinIdle(5); //最小闲置连接数
+            jedisPoolConfig.setBlockWhenExhausted(true); //连接耗尽是否等待
+            jedisPoolConfig.setMaxWaitMillis(2000); //等待时间
+            jedisPoolConfig.setTestOnBorrow(true); //取连接的时候进行一下测试 ping pong
+
+            jedisCluster=new JedisCluster(hostAndPortSet,jedisPoolConfig);
+            return  jedisCluster;
+        }else{
+            return  jedisCluster;
+        }
+
+    }
+
 }
